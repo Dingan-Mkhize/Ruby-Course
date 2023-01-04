9bc0f73485a567183f90cfa2183148f76ffd0756
dingan mkhize <>
2023-01-04 10:51:35 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+# puts "Enter a number to multiply by 2"
+# input = gets.chomp
+# puts input.to_i * 2
+
+# puts "What is your first name?"
+# first_name = gets.chomp
+# puts "What is your last name?"
+# last_name = gets.chomp
+# full_name = first_name + " " + last_name
+# puts "Your full name is #{full_name}"
+# puts "Your full name reversed is #{full_name.reverse}"
+# puts "Your name has #{full_name.length} letters"
+
+
