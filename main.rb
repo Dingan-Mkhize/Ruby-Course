3e21c0c47f8df1057a94b4c9d2d552d3b536e870
Dingan Mkhize <sunnydao@hotmail.co.uk>
2023-01-09 11:52:35 +0000

    password authenticator

--- main.rb
+++ main.rb
@@ -71,46 +71,84 @@
 #   first_num.to_f - second_num.to_f
 # end
 
-def add(first_num, second_num)
-  first_num.to_f + second_num.to_f
+# def add(first_num, second_num)
+#   first_num.to_f + second_num.to_f
+# end
+
+# def multiply(first_num, second_num)
+#   first_num.to_f * second_num.to_f
+# end
+
+# def divide(first_num, second_num)
+#   first_num.to_f / second_num.to_f
+# end
+
+# def subtract(first_num, second_num)
+#   first_num.to_f - second_num.to_f
+# end
+
+# def mod(first_num, second_num)
+#   first_num.to_f % second_num.to_f
+# end
+
+# puts "Simple Calculator"
+# 25.times { print "-" }
+# puts
+# puts "Enter the first number"
+# first_num = gets.chomp
+# puts "Enter the second number"
+# second_num = gets.chomp
+# puts "What do you want to do?"
+# puts "Enter 1 to multiply, 2 for addition, 3 for subtraction, 4 to divide, 5 to modulate the remainder"
+# user_entry = gets.chomp
+# if user_entry == "1"
+#   puts "You have chosen to multiply: #{first_num.to_f * second_num.to_f}"
+#   elsif user_entry == "2"
+#   puts "You have chosen to add: #{first_num.to_f + second_num.to_f}"
+#   elsif user_entry == "3"
+#   puts "you have chosen to subtract: #{first_num.to_f - second_num.to_f}"
+#   elsif user_entry == "4"
+#   puts "You have chosen to divide: #{first_num.to_f / second_num.to_f}"
+#   elsif user_entry == "5"
+#   puts "You have chosen to mod for the remainder: #{first_num.to_f % second_num.to_f}"
+# else
+#   puts "Invalid entry"
+# end
+
+users = [
+  { username: "dingan", password: "password1" },
+  { username: "jeffro", password: "password2" },
+  { username: "helena", password: "password3" },
+  { username: "monique", password: "password4" },
+  { username: "larry", password: "password5" },
+]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+  "Credentials were not valid"
 end
 
-def multiply(first_num, second_num)
-  first_num.to_f * second_num.to_f
-end
+puts "Welcome to the authenticator"
+25.times { print "-"}
+puts 
+puts "This program will take input from the user and compare with password"
+puts "If password is correct, the user will grt back the user object"
 
-def divide(first_num, second_num)
-  first_num.to_f / second_num.to_f
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or ay other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
 end
-
-def subtract(first_num, second_num)
-  first_num.to_f - second_num.to_f
-end
-
-def mod(first_num, second_num)
-  first_num.to_f % second_num.to_f
-end
-
-puts "Simple Calculator"
-25.times { print "-" }
-puts
-puts "Enter the first number"
-first_num = gets.chomp
-puts "Enter the second number"
-second_num = gets.chomp
-puts "What do you want to do?"
-puts "Enter 1 to multiply, 2 for addition, 3 for subtraction, 4 to divide, 5 to modulate the remainder"
-user_entry = gets.chomp
-if user_entry == "1"
-  puts "You have chosen to multiply: #{first_num.to_f * second_num.to_f}"
-  elsif user_entry == "2"
-  puts "You have chosen to add: #{first_num.to_f + second_num.to_f}"
-  elsif user_entry == "3"
-  puts "you have chosen to subtract: #{first_num.to_f - second_num.to_f}"
-  elsif user_entry == "4"
-  puts "You have chosen to divide: #{first_num.to_f / second_num.to_f}"
-  elsif user_entry == "5"
-  puts "You have chosen to mod for the remainder: #{first_num.to_f % second_num.to_f}"
-else
-  puts "Invalid entry"
-end
+puts "You have exceeded the maximum number of attempts" if attempts == 4
